8a71a799a73be7a8bad3c39c7afa0428bd6ef47e
renjiyun <wozaijixu@msn.cn>
2023-11-06 22:25:38 +0800

    新增 soot cfg 相关示例

---
+++ plugins/sonar-cia-plugin/src/main/java/org/sonar/plugins/cia/sootdemo/UnitGraphDemo.java
@@ -0,0 +1,46 @@
+package org.sonar.plugins.cia.sootdemo;
+
+import soot.*;
+import soot.options.Options;
+import soot.toolkits.graph.BriefUnitGraph;
+import soot.util.dot.DotGraph;
+
+/**
+ * @author renjiyun
+ */
+public class UnitGraphDemo {
+    public static void main(String[] args) {
+        Options.v().set_prepend_classpath(true);
+        Options.v().set_allow_phantom_refs(true);
+        Options.v().set_soot_classpath(
+                "plugins/sonar-cia-plugin/build/classes/java/main/org/sonar/plugins/cia/sootdemo");
+
+        Scene scene = Scene.v();
+        scene.loadClassAndSupport("FizzBuzz");
+        scene.loadNecessaryClasses();
+        SootClass sootClass = scene.getSootClass("FizzBuzz");
+        SootMethod sootMethod = sootClass.getMethodByName("printFizzBuzz");
+        briefUnitGraph(sootMethod);
+    }
+
+    /**
+     * draw the BriefUnitGraph of the method
+     *
+     * @param method
+     */
+    private static void briefUnitGraph(SootMethod method) {
+        String fileName = method.getName() + "-CFG-BUG";
+        DotGraph dot = new DotGraph(fileName);
+        BriefUnitGraph cfg = new BriefUnitGraph(method.retrieveActiveBody());
+        for (Unit unit : cfg) {
+            String unitHash = String.valueOf(unit.toString().hashCode());
+            dot.drawNode(unitHash).setLabel(unit.toString());
+
+            for (Unit succUnit : cfg.getSuccsOf(unit)) {
+                String succUnitHash = String.valueOf(succUnit.toString().hashCode());
+                dot.drawEdge(unitHash, succUnitHash);
+            }
+        }
+        dot.plot("plugins/sonar-cia-plugin/build/tmp/" + fileName + ".dot");
+    }
+}
