d3965d57bc2a1be96882c0abd9c9b8637e3352c2
renjiyun <wozaijixu@msn.cn>
2023-11-06 21:38:57 +0800

    添加 soot 使用示例

--- plugins/sonar-cia-plugin/src/main/java/org/sonar/plugins/cia/SootDemo.java
+++ plugins/sonar-cia-plugin/src/main/java/org/sonar/plugins/cia/SootDemo.java
@@ -1,8 +1,24 @@
 package org.sonar.plugins.cia;
 
+import soot.Body;
 import soot.Scene;
+import soot.SootClass;
+import soot.SootMethod;
+import soot.options.Options;
 
 public class SootDemo {
     public static void main(String[] args) {
+        Options.v().set_prepend_classpath(true);
+        Options.v().set_allow_phantom_refs(true);
+        Options.v().set_soot_classpath("plugins/sonar-cia-plugin/build/classes/java/main/org/sonar/plugins/cia");
+
+        Scene scene = Scene.v();
+        scene.loadClassAndSupport("FizzBuzz");
+        scene.loadNecessaryClasses();
+        SootClass sootClass = scene.getSootClass("FizzBuzz");
+        SootMethod sootMethod = sootClass.getMethodByName("printFizzBuzz");
+        Body body = sootMethod.retrieveActiveBody();
+
+        System.out.println(body);
     }
 }
