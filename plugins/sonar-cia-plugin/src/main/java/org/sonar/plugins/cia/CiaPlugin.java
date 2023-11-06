66c46bde3980169093c73c716756ed34a5581017
renjiyun <1277260264@qq.com>
2023-11-06 17:30:18 +0800

    引入 soot 框架

---
+++ plugins/sonar-cia-plugin/src/main/java/org/sonar/plugins/cia/CiaPlugin.java
@@ -0,0 +1,15 @@
+package org.sonar.plugins.cia;
+
+import org.sonar.api.Plugin;
+
+/**
+ * 变更影响分析插件
+ *
+ * @author renjiyun
+ */
+public class CiaPlugin implements Plugin {
+    @Override
+    public void define(Context context) {
+
+    }
+}
---
+++ plugins/sonar-cia-plugin/src/main/java/org/sonar/plugins/cia/SootDemo.java
@@ -0,0 +1,8 @@
+package org.sonar.plugins.cia;
+
+import soot.Scene;
+
+public class SootDemo {
+    public static void main(String[] args) {
+    }
+}
